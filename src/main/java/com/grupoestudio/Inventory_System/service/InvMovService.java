1aab694353dc975d057b98ffee8abd7f56385028
matias <mati.urra.94@gmail.com>
2025-04-29 23:53:18 -0400

    Refactor InvMovService and InvMovController for improved code structure and add stock management logic in save method

--- src/main/java/com/grupoestudio/Inventory_System/service/InvMovService.java
+++ src/main/java/com/grupoestudio/Inventory_System/service/InvMovService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.grupoestudio.Inventory_System.model.InventoryMovement;
+import com.grupoestudio.Inventory_System.model.Product;
 import com.grupoestudio.Inventory_System.repository.InvMovRepository;
+import com.grupoestudio.Inventory_System.repository.ProductRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -17,6 +19,9 @@ public class InvMovService {
     @Autowired
     private InvMovRepository invMovRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     public List<InventoryMovement> findallInvMov(){
         return invMovRepository.findAll();
     }
@@ -25,13 +30,32 @@ public class InvMovService {
         return invMovRepository.findById(id).orElseThrow(()-> new RuntimeException("Inventory Movement not found"));
     }
 
-    public InventoryMovement saveInvMov(InventoryMovement InvMov){
-        return invMovRepository.save(InvMov);
+    
+    public InventoryMovement save(InventoryMovement invMov){
+        Product product = productRepository.findById(invMov.getProduct().getId()).get();
+        /*Product productAsociado = invMov.getProduct();
+        Long idProductoAsociado = productAsociado.getId();
+        Product productAsociadoFinal = productRepository.findById(idProductoAsociado).get();*/
+        int currentStock = product.getStock();
+        if (invMov.getType().equals("ENTRADA")) {
+            product.setStock(currentStock + invMov.getQuantity());
+        }else if (invMov.getType().equals("SALIDA")) {
+            if (currentStock < invMov.getQuantity()) {
+                throw new RuntimeException("Te falta stock wn");
+            } else if (currentStock > invMov.getQuantity()){
+                throw new RuntimeException("Te sobra stock wn");
+            }
+            
+            product.setStock(currentStock - invMov.getQuantity());
+           
+        }
+
+        productRepository.save(product);
+        return invMovRepository.save(invMov);
     }
 
     public void deleteById(Long id){
         invMovRepository.deleteById(id);
     }
-
 
 }
